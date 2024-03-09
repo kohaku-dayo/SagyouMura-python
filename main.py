0955fe7d82a0400394e709c23c968b0b51485274
admin-lingstudy <admin@lingstudy.com>
2024-03-09 18:20:25 +0900

    fourth commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import os
-from keep_alive import keep_alive
 
 client = discord.Client(intents=discord.Intents.default())
 
@@ -14,6 +13,4 @@ async def on_message(message):
     await message.add_reaction(emoji)
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-# Web サーバの立ち上げ
-keep_alive()
 client.run(TOKEN)
